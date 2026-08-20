9b65b34a7c24714dd91bc5e306017cf11cf03afc
i-md-zaid <mohdzaid805505@gmail.com>
2026-08-20 04:22:40 +0000

    Add Scripting File Of Day1

---
+++ Network-info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Network information"
+echo "-------------------"
+
+ifconfig
+echo "-------------------"
+
+ip addr
+echo "-------------------"
+
+ip link
+echo "-------------------"
+
+
