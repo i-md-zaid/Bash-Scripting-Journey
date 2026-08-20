4cbd7e4436b514375ac789cf02a88492f90bad78
i-md-zaid <mohdzaid805505@gmail.com>
2026-08-20 10:12:48 +0000

    Add Scripting File Of Day2

---
+++ op.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Arithematic Operations"
+echo "----------------------"
+
+
+a=20
+b=10
+
+echo "Addition: $((a + b))"
+
+echo "Subrtaction: $((a - b))"
+
+echo "Multiplication: $((a * b))"
+
+echo "Division: $((a / b))"
+
+echo "Reminder: $((a % b))"
+
